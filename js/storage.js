9857456a6661437d9f2b782ab6a1fbcf721726d0
NikolajMemic <nikolajmemic78@gmail.com>
2026-04-20 11:01:36 +0200

    Delete js directory

--- js/storage.js
+++
@@ -1,9 +0,0 @@
-let high = localStorage.getItem("high") || 0;
-document.getElementById("high").innerText = high;
-
-function saveHigh(score){
-    if(score > high){
-        localStorage.setItem("high", score);
-        alert("NEW HIGH SCORE!");
-    }
-}
